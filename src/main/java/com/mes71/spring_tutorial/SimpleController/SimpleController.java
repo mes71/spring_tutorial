b2ac9b89a9dc58c5110be7bc3afdf921eb0262d1
mojtaba <mr.mojtaba47@gmail.com>
2025-02-18 15:48:05 +0330

    read : S67 until S68

--- src/main/java/com/mes71/spring_tutorial/SimpleController/SimpleController.java
+++ src/main/java/com/mes71/spring_tutorial/SimpleController/SimpleController.java
@@ -1,11 +1,21 @@
 package com.mes71.spring_tutorial.SimpleController;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SimpleController {
+
+
+    @Value("${instructor.name}")
+    String instructorName;
+
+
+    @Value("${student.class}")
+    String studentClass;
+
 
 
     @GetMapping("/")
@@ -17,5 +27,11 @@ public class SimpleController {
     @GetMapping("/goodbye")
     String sayGoodbye() {
         return "Goodbye World";
+    }
+
+
+    @GetMapping("/classInfo")
+    String getClassInfo(){
+        return  "Instructor: " + instructorName + " Student Class: " + studentClass;
     }
 }
