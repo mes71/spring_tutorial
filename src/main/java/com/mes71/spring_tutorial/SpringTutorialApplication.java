f9067da6c8bfc8afa0527827a1ee016adbdddde2
mojtaba <mr.mojtaba47@gmail.com>
2025-02-18 11:48:17 +0330

    Initial commit

---
+++ src/main/java/com/mes71/spring_tutorial/SpringTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.mes71.spring_tutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTutorialApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringTutorialApplication.class, args);
+    }
+
+}
